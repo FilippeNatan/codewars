8fea424864944fe0450df42034f6ed4eb058b444
Dev Zeus 1 <dev@zeus.clinic>
2023-07-18 13:33:00 -0300

    Commit inicial

---
+++ code-wars/8kyu/Disemvowel/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+Console.WriteLine(Kata.Disemvowel("Seu site é muito ruim e deveria acabar!"));
+
+public static class Kata
+{
+    public static string Disemvowel(string str)
+    {
+        string finalStr = "";
+        string vogais = "aeiou";
+        for(int x=0; x < str.Length; x++)
+        {
+            if(!vogais.Contains(Char.ToLower(str[x])))
+            {
+                finalStr += str[x];
+            }
+        }
+        str = finalStr;
+        return str;
+    }
+}

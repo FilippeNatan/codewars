7952a366290259787b2f50c0118a6065b4f6a45a
Dev Zeus 1 <dev@zeus.clinic>
2023-07-19 15:19:51 -0300

    DnaStrand

---
+++ code-wars/7kyu/ComplementaryDNA/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine(DnaStrand.MakeComplement("GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA"));
+
+public class DnaStrand 
+    {
+        public static string MakeComplement(string dna)
+        {
+            string dnaFinal = "";
+            foreach(char dnaCode in dna)
+            {
+                switch (dnaCode)
+                {
+                    case 'A':
+                        dnaFinal += 'T';
+                    break;
+                    case 'T':
+                        dnaFinal += 'A';
+                    break;
+                    case 'C':
+                        dnaFinal += 'G';
+                    break;
+                    case 'G':
+                        dnaFinal += 'C';
+                    break;
+                }
+            }
+            return dnaFinal;
+        }
+    }

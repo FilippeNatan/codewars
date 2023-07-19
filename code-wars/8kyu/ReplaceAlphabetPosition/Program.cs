fd43d969946f71012138e0deb82eda120a69c2d0
Dev Zeus 1 <dev@zeus.clinic>
2023-07-18 22:00:21 -0300

    AlphabetPosition

---
+++ code-wars/8kyu/ReplaceAlphabetPosition/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(Kata.AlphabetPosition("The narwhal bacons at midnight."));
+
+public static class Kata
+{
+  public static string AlphabetPosition(string text)
+  {
+    string sentence = "";
+    foreach(char character in text)
+    {
+        if(char.IsLetter(character))
+        {
+            int index = (int)character % 32;
+            sentence += index+" ";
+        }
+    }
+    if(sentence.Length > 0)
+    {
+        sentence = sentence[..^1];
+    }
+    text = sentence;
+    return text;
+  }
+}

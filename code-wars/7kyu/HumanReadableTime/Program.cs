eaad99bf14b1c84ca07b89ecb52eba8d34710b49
Dev Zeus 1 <filippe_natan@hotmail.com>
2023-07-20 09:51:14 -0300

    HumanReadableTime

---
+++ code-wars/7kyu/HumanReadableTime/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Write seconds to get readable time:");
+string? seconds = Console.ReadLine();
+Console.WriteLine(TimeFormat.GetReadableTime(Convert.ToInt32(seconds)));
+
+public static class TimeFormat
+{
+    public static string GetReadableTime(int seconds)
+    {
+        int hours = 0;
+        int minutes = 0;
+
+        if(seconds >= 3600)
+        {
+            hours = seconds/3600;
+            seconds = seconds - (3600 * hours);
+        }
+        if(seconds >= 60)
+        {
+            minutes = seconds/60;
+            seconds = seconds - (60 * minutes);
+        }
+        
+        string? time = $"{hours.ToString("D2")}:{minutes.ToString("D2")}:{seconds.ToString("D2")}";
+        return time;
+    }
+}

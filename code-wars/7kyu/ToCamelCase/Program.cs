6c3962ccc0a3c7fa3c31d2592910b37b867c6ab8
Dev Zeus 1 <filippe_natan@hotmail.com>
2023-07-27 11:05:14 -0300

    ToCamelCase

---
+++ code-wars/7kyu/ToCamelCase/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+Console.WriteLine(Kata.ToCamelCase("the_stealth_warrior"));
+Console.WriteLine(Kata.ToCamelCase("the-stealth-warrior"));
+Console.WriteLine(Kata.ToCamelCase("The_Stealth_Warrior"));
+Console.WriteLine(Kata.ToCamelCase("The_Stealth-Warrior"));
+
+public class Kata
+{
+  public static string ToCamelCase(string str)
+  {
+    bool firstLetter = false;
+    bool firstTime = true;
+    string finalStr = "";
+    char currentLetter;
+
+    foreach(char letter in str)
+    {
+        if(firstTime == true)
+        {
+            finalStr += letter;
+            firstTime = false;
+        }
+        else
+        {
+            if(char.IsLetter(letter))
+            {
+                if(firstLetter == true)
+                {
+                    currentLetter = char.ToUpper(letter);
+                    firstLetter = false;
+                }
+                else
+                {
+                    currentLetter = char.ToLower(letter);
+                }
+                finalStr += currentLetter;
+            }
+            else
+            {
+                firstLetter = true;
+            };
+        }
+        
+    }
+
+    return finalStr;
+  }
+}

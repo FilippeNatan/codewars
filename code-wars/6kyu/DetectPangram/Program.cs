619ffd492f423850e840b08fa24470bcbec4b968
Dev Zeus 1 <filippe_natan@hotmail.com>
2023-07-28 21:53:17 -0300

    IsPangram

---
+++ code-wars/6kyu/DetectPangram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+Console.WriteLine(Kata.IsPangram("Cwm fjord bank glyphs vext quiz"));
+public static class Kata
+{
+  public static bool IsPangram(string str)
+  {
+    char firstLetter = 'a';
+    List<char> alphabet = new List<char>();
+    while(Char.IsLetter(firstLetter))
+    {
+        alphabet.Add(firstLetter);
+        firstLetter++;
+    }
+
+    foreach(char letter in str)
+    {
+        if(alphabet.Contains(Char.ToLower(letter)))
+        {
+            alphabet.Remove(Char.ToLower(letter));
+        }
+    }
+    
+    switch(alphabet.Count())
+    {
+        case < 1:
+        return true;
+        
+        default:
+        return false;
+    }
+  }
+}
